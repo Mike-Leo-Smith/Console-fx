93f3dc57905ff97e705c4787e602f369edaaed41
mike <mango@live.cn>
2017-02-16 15:23:43 +0800

    Moved some marcos into "config.h".

---
+++ Console/Headers/config.h
@@ -0,0 +1,22 @@
+//
+// Created by Mike Smith on 2017/2/16.
+//
+
+#ifndef CONSOLE_FX_CONFIG_H
+#define CONSOLE_FX_CONFIG_H
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_WIDTH_RSHIFT 4
+
+#define VRAM_SIZE_BYTE 1024
+
+#define FONT_WIDTH 6
+#define FONT_HEIGHT 4
+#define FONT_DEPTH 4
+#define FONT_SIZE_BYTE 8
+
+#define ERROR_STACK_CAPACITY 16
+#define STRING_CAPACITY_BLOCK 8
+
+#endif //CONSOLE_FX_CONFIG_H
