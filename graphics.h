a3766da6433ce8728e0b138321139db2003bf981
mike <mango@live.cn>
2017-02-18 09:31:49 +0800

    Moved graphical functions into a separated header.

---
+++ graphics.h
@@ -0,0 +1,13 @@
+//
+// Created by Mike Smith on 2017/2/17.
+//
+
+#ifndef CONSOLE_FX_GRAPHICS_H
+#define CONSOLE_FX_GRAPHICS_H
+
+#include "glfw/glfw3.h"
+
+extern void Render(void);
+extern GLFWwindow *win;
+
+#endif //CONSOLE_FX_GRAPHICS_H
